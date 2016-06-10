8363f4f102dc95822ee3c0a1fbb119768e25173a
fantasmos <benji.k@live.com.au>
2016-06-10 16:04:38 +1000

    Beginning to Establish UserHandlers
    Created class that will be overridden, mostly compatible with previous
    userhandlers, however the new classes haven't been implemented yet.

---
+++ VBot.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SteamKit2;
+
+namespace SteamBotLite
+{
+    class VBot : UserHandler
+    {
+        public VBot(Bot bot, SteamID sid) : base(bot, sid){}
+        public override void OnMessage(string message, SteamKit2.EChatEntryType type) { }
+        public override void OnChatRoomMessage(SteamID chatID, SteamID sender, string message) { }
+        public override void OnLoginCompleted(){}
+    }
+}
