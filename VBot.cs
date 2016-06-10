4ef94f72e5de3b673433e915419edadf34b0b773
Fantasmos <andromedam51413>
2016-06-10 16:27:24 +1000

    Showing compatibility, New VBOT is nearly ready

--- VBot.cs
+++ VBot.cs
@@ -10,8 +10,21 @@ namespace SteamBotLite
     class VBot : UserHandler
     {
         public VBot(Bot bot) : base(bot){}
-        public override void OnMessage(SteamFriends.FriendMsgCallback ChatMsg) { }
-        public override void OnChatRoomMessage(SteamFriends.ChatMsgCallback ChatMsg) { }
-        public override void OnLoginCompleted(){}
+        public override void OnLoginCompleted() { }
+
+        public override void OnMessage(SteamFriends.FriendMsgCallback ChatMsg) //This is an example of using older methods for cross-compatibility, by converting the new format to the older one
+        {
+            OnMessage(ChatMsg.Message, ChatMsg.EntryType);       
+        }
+        public override void OnChatRoomMessage(SteamFriends.ChatMsgCallback ChatMsg) //This is an example of using older methods for cross-compatibility, by converting the new format to the older one
+        {
+            OnChatRoomMessage(ChatMsg.ChatRoomID, ChatMsg.ChatterID, ChatMsg.Message);
+        }
+        
+        public override void OnChatRoomMessage(SteamID chatID, SteamID sender, string message)
+        { }
+        public override void OnMessage(string message, SteamKit2.EChatEntryType type)
+        { }
+        
     }
 }
