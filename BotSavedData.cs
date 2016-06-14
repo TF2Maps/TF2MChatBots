5701c38b9954fddcb50ea235f4383dfe97e6ec87
Fantasmos <andromedam51413>
2016-06-14 14:24:38 +1000

    Getting Saved Data working

---
+++ BotSavedData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using SteamKit2;
+using System.IO;
+
+namespace SteamBotLite
+{
+    public class SteamBotData
+    {
+        SteamUser.LogOnDetails LoginData = new SteamUser.LogOnDetails();
+
+        public string username
+        {
+            get
+            {
+                return LoginData.Username;
+            }
+            set
+            {
+                Console.WriteLine("Username {0}", username);
+                LoginData.Username = value;
+            }
+        }
+        public string password
+        {
+            get
+            {
+                return LoginData.Password;
+            }
+            set
+            {
+                LoginData.Password = value;
+            }
+        }
+        string BotControlClass { get; set; }
+        
+
+    }
+}
