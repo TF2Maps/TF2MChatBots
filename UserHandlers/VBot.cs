5ba981de84f1d89468e5e19e33f4cd168def47a3
Fantasmos <andromedam51413>
2016-07-17 15:53:32 +1000

    Crashed, so I'm turning off servermodule

--- UserHandlers/VBot.cs
+++ UserHandlers/VBot.cs
@@ -48,11 +48,11 @@ namespace SteamBotLite
             mapModule = new MapModule(this, JsonConvert.DeserializeObject<Dictionary<string, object>>(jsconfig["MapModule"].ToString()));
             mapModule.mapList.CollectionChanged += OnMaplistchange;
 
-            serverModule = new ServerModule(this, JsonConvert.DeserializeObject<Dictionary<string, object>>(jsconfig["ServerModule"].ToString()));
+         //   serverModule = new ServerModule(this, JsonConvert.DeserializeObject<Dictionary<string, object>>(jsconfig["ServerModule"].ToString()));
             usersModule = new UsersModule(this, JsonConvert.DeserializeObject<Dictionary<string, object>>(jsconfig["usersModule"].ToString()));
             replyModule = new RepliesModule(this, JsonConvert.DeserializeObject<Dictionary<string, object>>(jsconfig["ReplyModule"].ToString()));
 
-            ModuleList = new List<BaseModule> { motdModule,mapModule,serverModule,usersModule,replyModule};
+            ModuleList = new List<BaseModule> { motdModule,mapModule,/*serverModule,*/usersModule,replyModule};
             
       //      serverModule.mapBeingTested += mapModule.HandleEvent;
 
