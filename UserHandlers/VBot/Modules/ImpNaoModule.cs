f3cf89dffe282556865e4299be29b7e52918d2b5
Fantasmos <andromedam51413>
2016-08-22 12:44:02 +1000

    Adding and map list receival

--- UserHandlers/VBot/Modules/ImpNaoModule.cs
+++ UserHandlers/VBot/Modules/ImpNaoModule.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 using System.Net.Http;
 using SteamKit2;
 using Newtonsoft.Json;
-
+using System.Linq;
 namespace SteamBotLite
 {
     class ImpNaoModule : BaseModule
@@ -13,13 +13,22 @@ namespace SteamBotLite
         public ImpNaoModule(VBot bot, Dictionary<string, object> Jsconfig) : base(bot, Jsconfig)
         {
             loadPersistentData();
-            /*
+            
             commands.Add(new Add(bot, this));
             commands.Add(new Maps(bot, this));
+            commands.Add(new Delete(bot, this));/*
             commands.Add(new Update(bot, this));
-            commands.Add(new Delete(bot, this));
             adminCommands.Add(new Wipe(bot, this));
             */
+        }
+
+        public class ImpNaoMap
+        {
+            public string Submitter { get; set; }
+            public string name { get; set; }
+            public string link { get; set; }
+            public string Notes { get; set; }
+            public string password { get; set; }
         }
 
         public override string getPersistentData()
@@ -59,34 +68,56 @@ namespace SteamBotLite
                 return SearchClass.CheckDataExistsOnWebPage(Website, MapName); //TODO develop method to check website
             }
 
-            public Add(VBot bot, ImpNaoModule Impnaomodule) : base(bot, "!imp", Impnaomodule) {
+            public Add(VBot bot, ImpNaoModule Impnaomodule) : base(bot, "!impadd", Impnaomodule) {
                 impnaomodule = Impnaomodule;
             }
 
             protected override string exec(SteamID sender, string param)
             {
-                impnaomodule.PostAsync("testmap", "Body");
+                string[] parameters = param.Split(new char[] { ' ' }, 2);
+
+                if (parameters.Length == 2)
+                {
+                    ImpNaoMap Map = new ImpNaoMap();
+                    Map.name = parameters[0];
+                    Map.link = parameters[1];
+                    Map.password = sender.ToString();
+                    impnaomodule.AddMapOnline(Map, "Body");
+                }
+                else
+                {
+                    return "Invalid Syntax";
+                }
+
+
+                
                 return "Completed";
             }
         }
 
-        public async Task PostAsync(string Map,string Body)
+        public async Task AddMapOnline(ImpNaoMap Map,string Body)
         {
             using (var client = new HttpClient())
             {
 
-                var values = new Dictionary<string, string>
-                        {
-                            { "name", "hello" },
-                            { "link", "world" },
-                            { "password", "elpsykongeroo"}
-                        };
-                string twent = JsonConvert.SerializeObject(values);
-
-                HttpContent Content = new StringContent(JsonConvert.SerializeObject(values));
-
-
+                HttpContent Content = new StringContent(JsonConvert.SerializeObject(Map));
                 var response = await client.PostAsync("http://carbidegames.com/impnao/api/maps", Content);
+                
+
+                var responseString = await response.Content.ReadAsStringAsync();
+            }
+        }
+
+        public async Task DeleteMapOnline(string Map)
+        {
+            using (var client = new HttpClient())
+            {
+                
+                HttpContent Content = new StringContent((Map));
+
+                string url = "http://carbidegames.com/impnao/api/maps/" + Map;
+
+                var response = await client.DeleteAsync("http://carbidegames.com/impnao/api/maps/" + Map);
 
                 var responseString = await response.Content.ReadAsStringAsync();
             }
@@ -94,9 +125,42 @@ namespace SteamBotLite
 
         private class Maps : MapCommand
         {
-            public Maps(VBot bot, ImpNaoModule impnaomodule) : base(bot, "!maps", impnaomodule) { }
+            public Maps(VBot bot, ImpNaoModule impnaomodule) : base(bot, "!impmaps", impnaomodule) { }
             protected override string exec(SteamID sender, string param)
             {
+                string ImpNaoPage = SearchClass.GetWebPageAsString("http://carbidegames.com/impnao/api/maps");
+                List <ImpNaoMap> mapList = JsonConvert.DeserializeObject<List<ImpNaoMap>>(ImpNaoPage);
+
+                string chatResponse = "";
+                string pmResponse = "";
+                int maxMaps = 10;
+
+                chatResponse = string.Join(" , ", mapList.Select(x => x.name));
+                if (mapList.Count > maxMaps)
+                    chatResponse += string.Format(" (and {0} more...)", mapList.Count - maxMaps);
+
+                // Build the private response.
+                pmResponse = "";
+                for (int i = 0; i < mapList.Count; i++)
+                {
+                    string mapLine = string.Format("{0} // {1} // {2} ({3})", mapList[i].name, mapList[i].link, userhandler.steamConnectionHandler.SteamFriends.GetFriendPersonaName(new SteamID(mapList[i].Submitter)), mapList[i].Submitter);
+
+                    if (!string.IsNullOrEmpty(mapList[i].Notes))
+                        mapLine += "\nNotes: " + mapList[i].Notes;
+
+                    if (i < mapList.Count - 1)
+                        mapLine += "\n";
+
+                    pmResponse += mapLine;
+                }
+                // PM map list to the caller.
+                if (mapList.Count != 0)
+                    userhandler.steamConnectionHandler.SteamFriends.SendChatMessage(sender, EChatEntryType.ChatMsg, pmResponse);
+
+                return chatResponse;
+
+
+
 
                 return "Completed";
             }
@@ -114,10 +178,15 @@ namespace SteamBotLite
 
         private class Delete : MapCommand
         {
-            public Delete(VBot bot, ImpNaoModule impnaomodule) : base(bot, "!delete", impnaomodule) { }
+            ImpNaoModule impnaomodule;
+            public Delete(VBot bot, ImpNaoModule module) : base(bot, "!impdelete", module)
+            {
+                impnaomodule = module;
+            }
             protected override string exec(SteamID sender, string param)
             {
-
+                string[] parameters = param.Split(new char[] { ' ' }, 2);
+                impnaomodule.DeleteMapOnline(parameters[0]);
                 return "Completed";
             }
 
