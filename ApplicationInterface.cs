84deec8d33683371c7f1d26e908aaba83a85a10f
fantasmos <benji.k@live.com.au>
2016-09-05 23:30:14 +1000

    Made Userhandler Methods Events instead

--- ApplicationInterface.cs
+++ ApplicationInterface.cs
@@ -50,8 +50,15 @@ namespace SteamBotLite
         public object identifier;
         public object extradata;
     }
-   
-    public abstract class ApplicationInterface
+
+    public class MessageProcessEventData : EventArgs
+    {
+        public UserIdentifier Sender;
+        public ChatRoomIdentifier Chatroom;
+        public string Message;        
+    }
+
+        public abstract class ApplicationInterface
     {
         public UserHandler Userhandler;
 
@@ -61,7 +68,66 @@ namespace SteamBotLite
         /* I beleive there is no benefit to making this role mandatory
         public abstract void ReceiveChatRoomMessage(ChatRoomIdentifier chatroomidentifier, string Message);
         public abstract void ReceivePrivateMessage(UserIdentifier useridentifier, string Message);
-        */ 
+        */
+
+        event EventHandler<MessageProcessEventData> ChatRoomMessageEvent;
+        
+        //The event-invoking method that derived classes can override.
+        protected virtual void ChatRoomMessageProcessEvent(MessageProcessEventData e)
+        {
+            // Make a temporary copy of the event to avoid possibility of
+            // a race condition if the last subscriber unsubscribes
+            // immediately after the null check and before the event is raised.
+            EventHandler<MessageProcessEventData> handler = ChatRoomMessageEvent;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+
+        event EventHandler<MessageProcessEventData> PrivateMessageEvent;
+
+        //The event-invoking method that derived classes can override.
+        protected virtual void PrivateMessageProcessEvent(MessageProcessEventData e)
+        {
+            // Make a temporary copy of the event to avoid possibility of
+            // a race condition if the last subscriber unsubscribes
+            // immediately after the null check and before the event is raised.
+            EventHandler<MessageProcessEventData> handler = PrivateMessageEvent;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+
+        event EventHandler<Tuple<UserIdentifier, bool>> ChatMemberInfoEvent;
+        //The event-invoking method that derived classes can override.
+        protected virtual void ChatMemberInfoProcessEvent(UserIdentifier e , bool isadmin)
+        {
+            // Make a temporary copy of the event to avoid possibility of
+            // a race condition if the last subscriber unsubscribes
+            // immediately after the null check and before the event is raised.
+            EventHandler<Tuple<UserIdentifier,bool>> handler = ChatMemberInfoEvent;
+            if (handler != null)
+            {
+                handler(this, new Tuple<UserIdentifier,bool>(e,isadmin));
+            }
+        }
+
+        public event EventHandler AnnounceLoginCompletedEvent;
+
+        protected virtual void AnnounceLoginCompleted()
+        {
+            // Make a temporary copy of the event to avoid possibility of
+            // a race condition if the last subscriber unsubscribes
+            // immediately after the null check and before the event is raised.
+            EventHandler<Tuple<UserIdentifier, bool>> handler = ChatMemberInfoEvent;
+            if (handler != null)
+            {
+                AnnounceLoginCompletedEvent(this , null);
+            }
+        }
+
         public abstract void ReceiveChatMemberInfo(UserIdentifier useridentifier, bool AdminStatus);
 
         public abstract void EnterChatRoom (ChatRoomIdentifier chatroomidentifier);
