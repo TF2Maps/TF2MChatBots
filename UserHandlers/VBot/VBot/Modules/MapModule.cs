033d68ad7d8a22e8c4456a8d94130227bb22c2f9
Fantasmos <benji.k@live.com.au>
2017-09-08 22:35:02 +1000

    Fixed deletion!

--- UserHandlers/VBot/VBot/Modules/MapModule.cs
+++ UserHandlers/VBot/VBot/Modules/MapModule.cs
@@ -686,9 +686,9 @@ namespace SteamBotLite
 
                 if (int.TryParse(parameters[0], out MapPositionInList)) {
                     
-                    if (MapPositionInList > MapModule.mapList.GetSize())
+                    if ((MapPositionInList > MapModule.mapList.GetSize()) | (MapPositionInList <= 0))
                     {
-                        return "That index does not exist!";
+                        return "That index does not exist! Please use a valid number when deleting maps";
                     }
                     else
                     {
@@ -712,7 +712,7 @@ namespace SteamBotLite
                     if ((deletedMap.IsOwner(Msg.Sender.identifier)) || (userhandler.admincheck(Msg.Sender)))
                     {
 
-                        MapModule.savePersistentData();
+                        
                         string Reason = "Deleted by " + Msg.Sender.DisplayName + " (" + Msg.Sender.identifier + "). ";
                         string ExplicitReason = param.Substring(parameters[0].Length, param.Length - parameters[0].Length);
 
@@ -728,7 +728,7 @@ namespace SteamBotLite
                         userhandler.SendPrivateMessageProcessEvent(new MessageEventArgs(null) { Destination = new User(deletedMap.Submitter, null), ReplyMessage = string.Format("Your map {0} has been deleted from the map list. {1}", deletedMap.Filename, Reason) });
 
                         MapModule.mapList.RemoveMap(deletedMap, Reason);
-
+                        MapModule.savePersistentData();
                         return string.Format("Map '{0}' DELETED. Sending: {1}", deletedMap.Filename, Reason);
                     }
                     else
